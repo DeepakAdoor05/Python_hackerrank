12f6efe81f8dd944ecc4d257519a71d6b1e714f1
DeepakAdoor05 <deepakadoor726@gmail.com>
2024-12-06 23:04:11 +0530

    List operations

--- List_operations.py
+++ List_operations.py
@@ -1,7 +1,18 @@
-if __name__ == '__main__':
-    N = int(input())
-arr = []
-i=0
-while i != N :
-    list=[input()]
-print(list)
+n = int(input())
+lst = []
+for i in range(n):
+    cmd = input().split()   # eg: append 6  => cmd=['append','6']
+    if cmd[0] == 'append':
+        lst.append(int(cmd[1]))
+    elif cmd[0] == 'insert':    #insert 0 5
+        lst.insert(int(cmd[1]),int(cmd[2]))
+    elif cmd[0] == 'remove':
+        lst.remove(int(cmd[1]))
+    elif cmd[0] == 'print':
+        print(lst)
+    elif cmd[0] == 'pop':
+        lst.pop()
+    elif cmd[0] == 'sort':
+        lst.sort()
+    else: 
+        lst.reverse()
