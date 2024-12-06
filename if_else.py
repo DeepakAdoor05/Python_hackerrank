0c83ee7a8bac739679ce7780dbef3c0e58fb9bbb
DeepakAdoor05 <deepakadoor726@gmail.com>
2024-12-06 23:40:41 +0530

    Using range function

--- if_else.py
+++ if_else.py
@@ -3,9 +3,9 @@ if n in range(1, 101):
     if n % 2 != 0:
         print("Weird")
     else:
-         if n in range(2, 6):
+         if n in range(2, 6):    # if n >= 2 and n <= 5:
             print("Not Weird")
-         elif n in range(6, 21):
+         elif n in range(6, 21): # elif n >= 6 and n <= 20:
             print("Weird")
-         else:
+         else:                   # elif n > 20:
             print("Not Weird")
