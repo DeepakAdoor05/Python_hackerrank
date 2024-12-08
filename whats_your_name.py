9bfcced239445234b845a8233f84d06f109b3866
DeepakAdoor05 <deepakadoor726@gmail.com>
2024-12-08 14:43:32 +0530

    What's Your Name?

---
+++ whats_your_name.py
@@ -0,0 +1,9 @@
+def print_full_name(first,last):
+    print(f"Hello {first} {last}! You just delved into python.")
+    # print("Hello {0} {1}! You just delved into python.".format(first,last))
+
+first_name = input()
+last_name = input()
+print_full_name(first_name,last_name)
+
+#Hello Ross Taylor! You just delved into python.
