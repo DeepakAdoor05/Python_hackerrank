a297710c1a4d5d262099d17e69f5c17c08340d68
DeepakAdoor05 <deepakadoor726@gmail.com>
2024-12-12 22:58:03 +0530

    String Validators

--- String_validators/string_validators1.py
+++ String_validators/string_validators1.py
@@ -4,3 +4,6 @@ print(any([i.isalpha() for i in s]))
 print(any([i.isdigit() for i in s]))    # any([False, False, True]) => True
 print(any([i.islower() for i in s]))
 print(any([i.isupper() for i in s]))
+
+# The any() function returns True if any item in an iterable are true, otherwise it returns False.
+# If the iterable object is empty, the any() function will return False.
