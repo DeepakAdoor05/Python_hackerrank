6a771efa590219e499a4825aaa5e7cb649e088f5
DeepakAdoor05 <deepakadoor726@gmail.com>
2025-02-21 15:28:30 +0530

    The Minion Game

--- The_Minion_Game/minion_game.py
+++ The_Minion_Game/minion_game.py
@@ -1,12 +1,12 @@
 def minion_game(string):
     s_count = 0
     k_count = 0
+    # vow = 'AEIOU'
     for i in range(len(string)):
         if string[i] in ['A','E','I','O','U']:
             k_count += len(string) - i
         else:
             s_count += len(string) - i
-    # print(s_count,k_count)
     if s_count > k_count:
         print("Stuart",s_count)
     elif s_count < k_count:
